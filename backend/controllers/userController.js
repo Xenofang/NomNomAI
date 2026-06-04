a0d69660d607f36f6a428828b5f25b33ecf02176
Xenofang <mandalshubham284@gmail>
2026-06-04 11:56:23 +0530

    add user existence check in createUser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,30 +9,35 @@ const generateToken = (id) => {
 };
 
 
-// register controller
 const createUser = async (req, res) => {
   try {
-
     const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
+
     const user = await userModel.create({
       name,
       email,
-      password: hash
+      password: hash,
     });
-    
-    console.log(hash);
-    res.json(user);
 
+    res.status(201).json(user);
   } catch (error) {
-
-    res.status(500).json({ message: error.message });
-
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
-
 
 // login controller
 const loginUser = async (req, res) => {
